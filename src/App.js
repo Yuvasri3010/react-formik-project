0157d700a5106e6bbc4619fde2be38a1f7fcb129
Yuvasri3010 <yuvasri6674@gmail.com>
2024-01-13 19:08:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+
 import './App.css';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 import Portal from './components/dashboard/Portal';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './components/dashboard/Dashboard';
---
+++ src/components/dashboard/Portal.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from './Navbar'
+
+
+
+
+
+
+
+
+function Portal() {
+    return (
+        <>
+            <Navbar />
+            <Outlet></Outlet>
+        </>
+    )
+}
+
+export default Portal
